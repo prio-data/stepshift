159131f35eed3c8e965d1e11d8b6adbd11a5c4b7
peder2911 <pglandsverk@gmail.com>
2021-07-09 15:35:26 +0200

    Wrote code for index-unit picking
    Wrote code that generates masks based on picks of random index units. This will be very useful for bootstrapping models with time / unit dropout.

--- stepshift.py
+++ stepshift.py
@@ -1,6 +1,8 @@
-from typing import Callable, Any, Union, Dict, List
+from typing import Callable, Any, Union, Dict, List, Generator
+from pymonad.either import Left, Right, Either
 import pandas as pd
 import numpy as np
+from numpy.typing import ArrayLike
 
 def stepshift(
         dataframe: pd.DataFrame,
@@ -25,3 +27,36 @@ def stepshift(
 def stepshifted(outcomes,inputs,steps,stepsize):
     for step in steps:
         yield outcomes[((step+1)*stepsize)-stepsize:], inputs[:-(step*stepsize),:]
+
+def index_draws(
+        level: int,
+        draw_size: int,
+        dataframe: pd.DataFrame)-> Either[str, Generator[ArrayLike, None, None]]:
+    def draws(which, from_what)-> Generator[ArrayLike, None, None]:
+        for pick in which:
+            mask = np.full(len(from_what),False)
+            for value in pick:
+                mask |= from_what == value
+            yield mask
+
+    try:
+        idx = np.array([i[level] for i in dataframe.index])
+    except TypeError:
+        return Left("Dataframe must have a multiindex")
+    except KeyError:
+        return Left(
+                "Dataframe had incorrect number of levels. "
+                "Needed level {level}, had "
+                "{len(dataframe.index[0])} levels."
+            )
+
+    unique_values = list(set(idx))
+    picks = np.linspace(0,len(unique_values)-1,len(unique_values))
+    np.random.shuffle(picks)
+
+    n_picks = len(picks)
+    picks = picks[:n_picks - (n_picks % draw_size)]
+    picks = picks.reshape(len(picks) // draw_size, draw_size)
+
+    return Right(draws(picks,idx))
+
