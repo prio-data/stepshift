bbf54d77e13da0a73549231897190d940730f238
peder2911 <pglandsverk@gmail.com>
2021-09-02 17:01:51 +0200

    Faster algo, better packaging
    Made the package compile properly on install, instead of having to do it
    with the dev. solution.
    
    In addition, helped by Cython profiling, I managed to significantly
    speed up the algo from the previous iteration (by a factor of 7)
    by removing some unnecessary python-calls.

--- stepshift/cast.pyx
+++ stepshift/cast.pyx
@@ -1,5 +1,6 @@
 cimport numpy as c_np
 import numpy as np
+import pandas as pd
 
 cdef (int, int) minmax(double[:] arr):
     cdef double min = np.inf
@@ -23,7 +24,7 @@ cdef (int) find_index(int idx, c_np.int_t[:] arr):
             break
     return i
 
-def time_unit_feature_cube(c_np.ndarray[double, ndim=2] raw_matrix):
+def time_unit_feature_cube(dataframe):
     """
     This function casts a matrix into a time_unit_feature cube,
     which is a useful transformation when indexing in the
@@ -34,38 +35,48 @@ def time_unit_feature_cube(c_np.ndarray[double, ndim=2] raw_matrix):
     are assumed to be FEATURES.
     """
 
+    # First some setup
+    cdef double[:,:] raw_matrix_view = (dataframe
+            .sort_index(level=[1,0]) # The DF is sorted to enable a much 
+            .reset_index()           # faster unit-indexing scheme.
+            .values
+            )
+
+    unique_units = np.unique(raw_matrix_view[:,1])
+
     # First figure out how to compute time-indices (min - i)
-    time_min,time_max = minmax(raw_matrix[:,0])
+    cdef int time_min
+    cdef int time_max 
+
+    time_min,time_max = minmax(raw_matrix_view[:,0])
     def time_index(int i):
         return <int> (time_min - i)
-
-    # Then, figure out how to compute unit indices
-    # i is equal to the index of the unit in an ordered array of the unique
-    # unit identifiers.
-    cdef c_np.int_t[:] unique_units
-    unique_units = np.unique(raw_matrix[:,1]).flatten().astype(int)
-    lookup = dict()
-    def unit_index(int i):
-        if i in lookup.keys():
-            return lookup[i]
-        idx = find_index(i, unique_units)
-        lookup[i] = idx
-        return idx
 
     # Create an empty cube for holding the result
     cdef c_np.ndarray[double, ndim=3] result_array
     result_array = np.full([
             (time_max - time_min)+1,
             len(unique_units), 
-            (raw_matrix.shape[1]-2)
+            (raw_matrix_view.shape[1]-2)
             ], np.inf, dtype = np.float64) 
+    cdef double[:,:,:] result_view = result_array
 
     # Then iterate over each row in the original dataset, adding the
     # values to the new cube
     cdef int i
     cdef double[:] row
-    for i in range(raw_matrix.shape[0]):
-        row = raw_matrix[i,:]
-        result_array[time_index(<int> row[0]), unit_index(<int> row[1]), :] = row[2:] 
+    cdef int t_index
+    cdef int u_index = -1 
+    cdef int current_u_index = -1 
+
+    for i in range(raw_matrix_view.shape[0]):
+        row = raw_matrix_view[i,:]
+
+        if row[1] != current_u_index:
+            current_u_index = <int> row[1]
+            u_index = u_index + 1
+
+        t_index = time_min - <int> row[0] 
+        result_view[t_index, u_index, :] = row[2:] 
 
     return result_array 
